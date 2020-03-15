c606e4f25f66be0bdd30a4b16174322c7ffa40c5
Keith <keith.blackwelder@onesourceits.com>
2020-03-15 14:30:17 -0400

    making changes

--- js/cancelEmail.js
+++
@@ -1,9 +0,0 @@
-/*
-** js file to  handle canceling an email sub **
-*/
-
-let targetBtn = document.getElementById('cancelSub');
-targetBtn.addEventListener('click', function () {
-    const emailData = location.search;
-    alert(emailData);
-});
